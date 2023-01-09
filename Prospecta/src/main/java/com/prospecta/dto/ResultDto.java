d9f6b129a82d5f0cee77724159ce270498f6202f
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-01-09 15:24:59 +0530

    done

---
+++ Prospecta/src/main/java/com/prospecta/dto/ResultDto.java
@@ -0,0 +1,32 @@
+package com.prospecta.dto;
+
+public class ResultDto {
+	
+	private String title;
+	
+	private String description;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public ResultDto() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ResultDto(String title, String description) {
+		super();
+		this.title = title;
+		this.description = description;
+	}
+
+}
---
+++ Prospecta/src/main/java/com/prospecta/service/EntriesService.java
@@ -0,0 +1,10 @@
+package com.prospecta.service;
+
+import com.prospecta.model.Entries;
+
+public interface EntriesService {
+	
+	
+	public String saveData(Entries entries);
+
+}
